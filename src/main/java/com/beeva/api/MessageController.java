462f56c721c116f042c7869e1b7ee12257a16aef
Jose Daniel Hernandez Osorio <josedaniel.hernandez.contractor@beeva.com>
2018-02-13 09:57:44 -0600

    init security-rest

---
+++ src/main/java/com/beeva/api/MessageController.java
@@ -0,0 +1,33 @@
+package com.beeva.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @GetMapping("/message")
+    public String messageGET() {
+        return "Hola GET";
+    }
+
+    @PostMapping("/message")
+    public String messagePOST() {
+        return "Hola POST";
+    }
+
+    @DeleteMapping("/message")
+    public String messageDELETE() {
+        return "Hola DELETE";
+    }
+
+    @PutMapping("/message")
+    public String messagePUT() {
+        return "Hola PUT";
+    }
+
+
+}
